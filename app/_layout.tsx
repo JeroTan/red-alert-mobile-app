77966ee754fba5fcc95db7fa4ee828754975001c
Jerowe Tan <j.tan@toyota-mobilitysolutions.ph>
2026-03-19 17:00:38 +0800

    chore: future update with the error logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack />;
+      <StatusBar style="auto" hidden />
+    </ThemeProvider>
+  );
 }
